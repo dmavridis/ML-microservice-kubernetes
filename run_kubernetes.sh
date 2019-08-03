849a27690e96b0d298204b2635904fb4622c7594
Dimitrios Mavridis <dmavridis@outlook.com>
2019-08-03 08:12:27 +0100

    Add files via upload

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=dmavridis/prediction
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run --generator=prediction --image=dockerpath:latest
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+
